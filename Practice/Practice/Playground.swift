907bc6b75c1ccdec3ef3916b84ba75b366ce76cc
patrickmckowen <pmckowen@hey.com>
2020-09-22 12:32:26 -0700

    Adding timer countdown animation

--- Practice/Practice/Playground.swift
+++ Practice/Practice/Playground.swift
@@ -8,14 +8,49 @@
 import SwiftUI
 
 struct Playground: View {
+    @EnvironmentObject var yogi: Yogi
+    @EnvironmentObject var appTimer: AppTimer
+    
+    var progress: Double {
+        let options = appTimer.lengthOptions
+        let index = appTimer.timePickerIndex
+        let minutes = options[index]
+        let seconds = Double(minutes * 60)
+        let countTo = seconds
+        
+        let timePassed = appTimer.timePassed
+        
+        return Double(timePassed / countTo)
+    }
+    
     var body: some View {
-        VStack(spacing: 8) {
-            Spacer()
-            Button("Log Session") { }
-                .buttonStyle(ButtonLight())
-            Button("Cancel") { }
-                .buttonStyle(ButtonLight())
-                .padding(.bottom, 32)
+        VStack(spacing: 0) {
+            ZStack {
+                // Background Stroke
+                Circle()
+                    .stroke(style: StrokeStyle(lineWidth: 5.0, lineCap: .round, lineJoin: .round))
+                    .frame(width: 144, height: 144)
+                    .foregroundColor(Color.white.opacity(0.5))
+                
+                // Filled Stroke
+                Circle()
+                    .trim(from: 0.0, to: CGFloat(min(progress, 1.0)))
+                    .stroke(style: StrokeStyle(lineWidth: 5.0, lineCap: .round, lineJoin: .round))
+                    .rotationEffect(Angle(degrees: 270.0))
+                    .frame(width: 144, height: 144)
+                    .foregroundColor(Color.white.opacity(1.0))
+                
+                Image(systemName:
+                        appTimer.state != .running
+                        ? "play.fill" : "pause.fill")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(maxWidth: 48, maxHeight: 48)
+                    .foregroundColor(.white)
+                    .shadow(color: Color/*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/.opacity(0.3), radius: 8, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: 4.0)
+                
+            }
+            
         }
         .background(Image("default")).edgesIgnoringSafeArea(.all)
     }
@@ -25,6 +60,10 @@ struct Playground: View {
 
 struct Playground_Previews: PreviewProvider {
     static var previews: some View {
-        Playground()
+        let appTimer = AppTimer()
+        let yogi = Yogi()
+        return Playground()
+            .environmentObject(appTimer)
+            .environmentObject(yogi)
     }
 }
--- Practice/Practice/TimerControls.swift
+++ Practice/Practice/TimerControls.swift
@@ -21,6 +21,18 @@ struct TimerControls: View {
         return options[index]
     }
     
+    var progress: Double {
+        let options = appTimer.lengthOptions
+        let index = appTimer.timePickerIndex
+        let minutes = options[index]
+        let seconds = Double(minutes * 60)
+        let countTo = seconds
+        
+        let timePassed = appTimer.timePassed
+        
+        return Double(timePassed / countTo)
+    }
+    
     var body: some View {
         ZStack {
             // Gradient
@@ -110,17 +122,29 @@ struct TimerControls: View {
                 ZStack {
                     // Play Button
                     ZStack {
+                        // Background Stroke
                         Circle()
-                            .frame(width: 124, height: 124)
-                            .foregroundColor(.white)
-                            .shadow(color: Color/*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/.opacity(0.08), radius: 8, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: 4.0)
+                            .stroke(style: StrokeStyle(lineWidth: 5.0, lineCap: .round, lineJoin: .round))
+                            .frame(width: 144, height: 144)
+                            .foregroundColor(Color.white.opacity(0.5))
+                        
+                        // Filled Stroke
+                        Circle()
+                            .trim(from: 0.0, to: CGFloat(min(progress, 1.0)))
+                            .stroke(style: StrokeStyle(lineWidth: 5.0, lineCap: .round, lineJoin: .round))
+                            .rotationEffect(Angle(degrees: 270.0))
+                            .frame(width: 144, height: 144)
+                            .foregroundColor(Color.white.opacity(1.0))
+                            .animation(.linear)
+                        
                         Image(systemName:
                                 appTimer.state != .running
                                 ? "play.fill" : "pause.fill")
                             .resizable()
                             .aspectRatio(contentMode: .fit)
-                            .frame(maxWidth: 40, maxHeight: 40)
-                            .foregroundColor(.black)
+                            .frame(maxWidth: 48, maxHeight: 48)
+                            .foregroundColor(.white)
+                            .shadow(color: Color/*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/.opacity(0.3), radius: 8, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: 4.0)
                         
                     }
                     .matchedGeometryEffect(id: "PlayButton", in: playAnimation)
